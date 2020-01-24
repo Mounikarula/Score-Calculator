e0e55c5697110456dafa0b2887298f0423e91924
unknown <mgangula@powercastco.com>
2020-01-24 14:14:43 -0500

    initial setup of Players score calculator using typescript

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function startGame() {
+	var messagesElement = document.getElementById('messages');
+	messagesElement.innerText = "WElcome to a new game.....";
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
