4d8661518818cc1e67894eb0850ee51ae896899e
unknown <mgangula@powercastco.com>
2020-01-25 22:19:49 -0500

    added bootstrap style sheet and typescript

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+"use strict";
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = "WElcome to a new game.....";
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
