03698fadfc0185e2d9e4354195ed1ddf21f663f9
dipo99 <mounika.rgula@gmail.com>
2020-01-26 23:57:10 -0500

    deleted unwanted js files

--- js/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = "WElcome to a new game.....";
-}
-document.getElementById('startGame').addEventListener('click', startGame);
-//# sourceMappingURL=app.js.map
