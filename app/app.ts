10092269b0480b55220e15c78312b529a27e0e56
dipo99 <mounika.rgula@gmail.com>
2020-01-26 23:50:32 -0500

    modified tsconfig and deleted js files

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,14 @@
 function startGame() {
+	
+	let playerName: string = "Mouni";
+	logPlayer(playerName);
+
 	var messagesElement = document.getElementById('messages');
-	messagesElement!.innerText = "WElcome to a new game.....";
+	messagesElement!.innerText = "Welcome to a new game.....";
+}
+
+function logPlayer(name) {
+	console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
